2308f3476a2ee5807ff3e0c13a6f91cf35d37003
6uiwoom <6uiwoom@gmail.com>
2024-12-18 17:29:32 +0900

    20241218 tailwind 적용 및 toLocaleString 수정

--- client/src/App.js
+++ client/src/App.js
@@ -47,38 +47,68 @@ function App() {
         });
   }
 
+  // 게시글 수정하기
+  
+
   // 컴포넌트가 처음 렌더링될 때 게시글 목록을 가져오기
   useEffect(() => {
     boardUpdate();
   }, []); // 빈 배열([])로 설정하여 한 번만 실행되도록
 
+  const formatDate = (dateStr) => {
+    const date = new Date(dateStr);
+    const options = {
+      year: 'numeric',
+      month : 'long',
+      day : 'numeric',
+      hour: '2-digit',
+      minute:'2-digit',
+      second: '2-digit',
+      hour12: true
+    };
+    return date.toLocaleString('ko-KR', options)
+   }
+
   return (
       <div className="App">
-        <div className='addListBox'>
-          <input
-              placeholder='제목'
-              onChange={getValue}
-              name='title'
-          />
-          <input
-              placeholder='내용'
-              onChange={getValue}
-              name='content'
-          />
-          <button onClick={submit}>등록</button>
+        <div className='max-w-4xl mx-auto p-6 bg-blue mb-6'>
+          <div className='flex flex-col space-y-4'>
+            <input
+                placeholder='제목'
+                onChange={getValue}
+                name='title'
+                className='border-2 border-gray-300 p-2 rounded-md w-full'
+
+            />
+            <input
+                placeholder='내용'
+                onChange={getValue}
+                name='content'
+                className='border-2 border-gray-300 p-2 rounded-md w-full'
+
+            />
+            <button onClick={submit}
+            className='bg-blue-500 text-white p-2 rounded-md '
+            >등록</button>
+          </div>
         </div>
 
         {viewContent.map((i, index) => {
-          return (
-              <div key={index} className='listBox'>
-                <p>{i.index}</p>
-                <p>{i.date}</p>
-                <p>{i.title}</p>
-                <p>{i.content}</p>
-                <button className="bg-green-400 p-4">수정</button>
-                <button>삭제</button>
+            return (
+              <div key={index} className="flex justify-between p-4 shadow-md ">
+                <div className="flex items-center mb-2 gap-6">
+                  <p className='text-lg text-gray-500'>{i.id}</p>
+                  <p className="text-lg font-semibold">{i.title}</p>
+                  <p className="text-gray-700 ">{i.content}</p>
+                </div>
+
+                  <div className="flex items-center gap-1 ">
+                    <p className="text-sm text-gray-500">{formatDate(i.date)}</p>
+                    <button className=" text-gray-500 px-1  rounded-md">수정</button>
+                    <button className=" text-gray-500 px-1  rounded-md">삭제</button>
+                  </div>
               </div>
-          )
+            )
         })}
       </div>
   );
