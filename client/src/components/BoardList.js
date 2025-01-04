625556ae75ee308459869a0e77e6afc70e291236
bokpack <6uiwoom@gmail.com>
2025-01-04 14:19:30 +0900

    20250104 페이지네이션 추가완료

--- client/src/components/BoardList.js
+++ client/src/components/BoardList.js
@@ -2,8 +2,19 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const BoardList = ({ posts }) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const postsPerPage = 10;
     const formatDate = (dateStr) => new Date(dateStr).toLocaleString("ko-KR");
     const navigate = useNavigate();
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    const totalPosts = posts.length;
+    const totalPages = Math.ceil(totalPosts / postsPerPage);
     const handleClick = (id) => {
         navigate(`/detail/${id}`)
     }
@@ -25,7 +36,7 @@ const BoardList = ({ posts }) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {posts.map((post) => (
+                    {currentPosts.map((post) => (
                         <tr key={post.id} onClick={() => handleClick(post.id)} className="cursor-pointer hover:bg-gray-200 text-center">
                             <td className="border-b p-2">{post.id}</td>
                             <td className="border-b p-2">{post.title}</td>
@@ -35,6 +46,19 @@ const BoardList = ({ posts }) => {
                     ))}
                 </tbody>
             </table>
+            <div className="flex justify-center space-x-2 p-4">
+                <button onClick={()=> paginate(currentPage - 1)} disabled={currentPage === 1} className="px-4 py-2 bg-gray-300 text-gray-700 rounded">
+                    이전
+                </button>
+                {[...Array(totalPages)].map((_, index) =>(
+                    <button key={index} onClick={()=> paginate(index + 1)} className={`px-4 py-2 rounded ${currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'}`}>
+                        {index + 1}
+                    </button>
+                ))}
+                <button onClick={() => paginate(currentPage + 1)} disabled={currentPage === totalPages} className="px-4 py-2 bg-gray-300 text-gray-700 rounded">
+                    다음
+                </button>
+            </div>
         </div>
         
     )
